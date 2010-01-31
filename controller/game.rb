b7a5f75542c1025dcd8861762b78ea337d615e0e
Taro Omiya <japtar10101@gmail.com>
2010-01-31 10:54:21 -0500

    adding controller directory

--- controller/game.rb
+++ controller/game.rb
@@ -7,8 +7,6 @@ require "model/group"
 require "model/spike"
 require "model/wall"
 require "model/status"
-require "model/menu"
-require "model/image"
 require "global"
 require "layout"
 
@@ -23,34 +21,60 @@ puts 'Warning, font disabled' unless
   ($font_ok = (VERSIONS[:sdl_ttf] != nil))
 puts 'Warning, sound disabled' unless
   ($sound_ok = (VERSIONS[:sdl_mixer] != nil))
-
-# main executable
-class TripTrap
+  
+# The Game class helps organize thing. It takes events
+# from the queue and handles them, sometimes performing
+# its own action (e.g. Escape key = quit), but also
+# passing the events to the pandas to handle.
+#
+class Game
   include EventHandler::HasEventHandler
-  def initialize()
-    make_screen [DOUBLEBUF]
+ 
+  def initialize(layout = nil)
+    make_screen [FULLSCREEN, DOUBLEBUF]
     make_clock
     make_queue
     make_event_hooks
-    @state = STATE_TITLE
-    @layout = LayoutGenerator.new
-    #@layout.load_file()
-    
-    
-    #@menu = Image.new
+    @ship = Ship.new( @screen.w/2, @screen.h/2, RESOLUTION )
+    make_magic_hooks_for( @ship, { YesTrigger.new() => :handle } )
+    @goons = Group.new
+    @spikes = Group.new
+    @walls = Group.new
+    if layout
+    	@layout = layout
+    else
+    	@layout = LayoutGenerator.new
+    end
+    @status = Status.new(@screen)
+    @layout.load_file('levels/cage.yaml')
+    load_layout
   end
-  
+ 
   # The "main loop". Repeat the #step method
   # over and over and over until the user quits.
-  def run
+  def go
     catch(:quit) do
       loop do
         step
       end
     end
   end
+ 
+  def load_layout()
+  	@layout.generate_layout(@ship, @goons, @walls, @spikes)
+    for goon in @goons 
+  		make_magic_hooks_for( goon, { YesTrigger.new() => :handle } )
+  	end
+  	for spike in @spikes 
+  		make_magic_hooks_for( spike, { YesTrigger.new() => :handle } )
+  	end
+  	for wall in @walls 
+  		make_magic_hooks_for( wall, { YesTrigger.new() => :handle } )
+  	end
+  end
   
   private
+ 
   # Create a new Clock to manage the game framerate
   # so it doesn't use 100% of the CPU
   def make_clock
@@ -59,6 +83,7 @@ class TripTrap
     @clock.calibrate
     @clock.enable_tick_events
   end
+ 
  
   # Set up the event hooks to perform actions in
   # response to certain events.
@@ -70,7 +95,7 @@ class TripTrap
  
     make_magic_hooks( hooks )
   end
-  
+ 
   # Create an EventQueue to take events from the keyboard, etc.
   # The events are taken from the queue and passed to objects
   # as part of the main loop.
@@ -88,18 +113,34 @@ class TripTrap
     @screen = Screen.open( RESOLUTION, 0, flags )
     @screen.title = "Trip Trap"
   end
-  
+ 
   # Quit the game
   def quit
     puts "Quitting!"
     throw :quit
   end
-  
+ 
   # Do everything needed for one frame.
   def step
     # Clear the screen.
     @screen.fill( :black )
     
+    # Check if the ship died
+    if @ship.is_dead?
+    	puts "You've been gooned!"
+    	quit
+    end
+    
+    # check if any goons are alive
+    num_goons = 0
+    for goon in @goons
+    	num_goons += 1 unless goon.dead
+    end
+    if num_goons == 0
+    	puts 'You won!'
+    	quit
+    end
+ 
     # Fetch input events, etc. from SDL, and add them to the queue.
     @queue.fetch_sdl_events
  
@@ -110,17 +151,59 @@ class TripTrap
     @queue.each do |event|
       handle( event )
     end
+ 
+    # have goons bounce off of each other
+    @goons.collide_group(@walls) do |goon, check|
+    	check_rect = check.rect
+    	goon_rect = goon.rect
+    	xdiff = (check_rect.centerx - goon_rect.centerx).abs
+    	ydiff = (check_rect.centery - goon_rect.centery).abs
+    	#reflect
+			if xdiff > ydiff
+				#goon.pushy = -1 * goon.ay
+				goon.vy *= -1
+			else
+				#goon.pushx = -1 * goon.ax
+				goon.vx *= -1
+			end
+    end
+    @goons.collide_self do |goon, check|
+    	check_rect = check.rect
+    	goon_rect = goon.rect
+    	xdiff = (check_rect.centerx - goon_rect.centerx).abs
+    	ydiff = (check_rect.centery - goon_rect.centery).abs
+    	#reflect
+			if xdiff > ydiff
+				#goon.pushy = -1 * goon.ay
+				goon.vy *= -1
+			else
+				#goon.pushx = -1 * goon.ax
+				goon.vx *= -1
+			end
+    end
     
-    # TODO: Draw everything
+    # have goons die on spikes
+    @spikes.collide_group(@goons) do |spike, goon|
+    	goon.dead = true
+    end
+    
+    # Draw everything
+    @status.render_text @ship.health.to_s, num_goons.to_s
+    @spikes.draw @screen
+    @walls.draw @screen
+    @goons.draw @screen
+    @ship.draw @screen
     
     # Refresh the screen.
     @screen.update()
   end
+ 
 end
-
+ 
 # Start the main game loop. It will repeat forever
 # until the user quits the game!
-TripTrap.new.run
+Game.new.go
  
 # Make sure everything is cleaned up properly.
 Rubygame.quit()
+
