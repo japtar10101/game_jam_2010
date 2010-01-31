6fcb1aa2b7ef2c6bf92f03dbfcbed40c1e09bb33
Taro Omiya <japtar10101@gmail.com>
2010-01-30 20:50:37 -0500

    compressing walls and added logo.

--- model/menu.rb
+++ model/menu.rb
@@ -18,8 +18,59 @@ class Menu
     	@layout = LayoutGenerator.new
     end
     @intro = true
+    
+    #store level data
     @levels = YAML.load_file filename
+    
+    #setup items and index to display
+    @items = Array.new(2)
+    set_items_to_intro
+    @index = 0
+    
+    #load an image
+    @image = Surface.load('sprites/spikes.png')
+    @image.fill(:white)
+    @rect = @image.make_rect
+    
+    # Create event hooks in the easiest way.
+    make_magic_hooks(
+ 
+      # Send keyboard events to #key_pressed() or #key_released().
+      KeyPressed => :key_pressed,
+      KeyReleased => :key_released,
+ 
+      # Send ClockTicked events to #update()
+      ClockTicked => :update
+    )
 	end
 	
+	private
+	def set_items_to_intro
+		@index = 0
+		@items.clear
+		@items << "Start"
+		@items << "How to Play"
+	end
 	
+	# Add it to the list of keys being pressed.
+  def key_pressed( event )
+    @keys += [event.key]
+  end
+ 
+ 
+  # Remove it from the list of keys being pressed.
+  def key_released( event )
+    @keys -= [event.key]
+  end
+	
+  # Update the ship state. Called once per frame.
+  def update( event )
+    dt = event.seconds # Time since last update
+ 
+		update_accel
+		update_vel dt 
+		update_pos dt
+		
+		@invulnerable -= 1 if @invulnerable > 0
+  end
 end
